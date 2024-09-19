7a722d01006dce6b7b2e504d3a67bdb39509bc17
Scanood <wangb.edu@gmail.com>
2024-09-19 15:13:30 +0800

    feat: 添加托盘单击显示主界面

--- src/mainprocess/app/tray.ts
+++ src/mainprocess/app/tray.ts
@@ -5,6 +5,9 @@ function SetupTray(mainWindow: BrowserWindow) {
     console.log(__dirname);
     const icon = nativeImage.createFromPath(path.join(__dirname, 'images/logo.png'))
     tray = new Tray(icon)
+    tray.on('click', () => {
+        mainWindow.show()
+    })
     const menu = Menu.buildFromTemplate([
         { label: '打开主页面', type: 'normal', click: () => { mainWindow.show() } },
         { label: '退出', type: 'normal', role: 'quit' },
